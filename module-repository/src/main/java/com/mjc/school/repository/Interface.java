d0e32d447e25f2120690f4aeb4d0bf78d3ce931c
m4loo <128167781+m4loo@users.noreply.github.com>
2024-01-21 19:27:59 +0500

    done?

---
+++ module-repository/src/main/java/com/mjc/school/repository/Interface.java
@@ -0,0 +1,15 @@
+package com.mjc.school.repository;
+
+import java.util.List;
+
+public interface Interface<T> {
+    List<T> readAll();
+
+    T readById(Long id);
+
+    T createNews(T model);
+
+    T updateNewsById(T model);
+
+    Boolean deleteById(Long id);
+}
